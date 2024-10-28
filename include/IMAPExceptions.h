637c621b8d0e49f554ac4b4b412e82c10581fdd3
xbonda06 <mediabondar@gmail.com>
2024-10-28 17:33:43 +0100

    feat(exceptions): add IMAP BAD and NO exceptions

---
+++ include/IMAPExceptions.h
@@ -0,0 +1,23 @@
+//
+// Created by xbonda06 on 28.10.2024.
+//
+
+#ifndef IMAP_TLS_CLIENT_IMAPEXCEPTIONS_H
+#define IMAP_TLS_CLIENT_IMAPEXCEPTIONS_H
+
+#include <stdexcept>
+#include <string>
+
+class IMAPNoResponseException : public std::runtime_error {
+public:
+    explicit IMAPNoResponseException(const std::string& message)
+            : std::runtime_error("IMAP NO Response: " + message) {}
+};
+
+class IMAPBadResponseException : public std::runtime_error {
+public:
+    explicit IMAPBadResponseException(const std::string& message)
+            : std::runtime_error("IMAP BAD Response: " + message) {}
+};
+
+#endif //IMAP_TLS_CLIENT_IMAPEXCEPTIONS_H
---
+++ include/IMAPResponceType.h
@@ -0,0 +1,15 @@
+//
+// Created by xbonda06 on 28.10.2024.
+//
+
+#ifndef IMAP_TLS_CLIENT_IMAPRESPONCETYPE_H
+#define IMAP_TLS_CLIENT_IMAPRESPONCETYPE_H
+
+enum class IMAPResponseType {
+    OK,
+    NO,
+    BAD,
+    UNKNOWN
+};
+
+#endif //IMAP_TLS_CLIENT_IMAPRESPONCETYPE_H
